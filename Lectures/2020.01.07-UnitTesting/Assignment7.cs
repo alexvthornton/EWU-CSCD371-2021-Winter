0eb8e0a583ac31c10b6f40284edae1e40434a193
Alex Thornton <athornton4@eagles.ewu.edu>
2021-03-02 22:00:13 -0800

    added support to DownloadTextRepeatedlyAsync for observing the progress but used normal for loop not parallel

---
+++ Lectures/2020.01.07-UnitTesting/Assignment7.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+
+namespace Assignment7
+{
+
+    static class methods
+    {
+        private static Object obj = new object();
+
+        public static async Task<int> DownloadTextAsync(params string[] urls)
+        {
+            // calls System.Next.WebClient.DownloadString() 
+            // returns total number of characters downloaded from all urls
+            var client = new WebClient();
+
+            return await Task.Run(() =>
+            {
+                int total = 0;
+                foreach (string url in urls)
+                {
+                    total += client.DownloadString(url).Length;
+                }
+                return total;
+            });
+
+        }
+
+
+        public static async Task<int> DownloadTextRepeatedlyAsync(int repetitions, IProgress<double> progress, params string[] urls)
+        {
+            return await Task.Run( () =>
+            {
+                int total = 0;
+                int count = 1;
+                Parallel.For(0, repetitions, async i =>
+                {
+                    
+                    total += await DownloadTextAsync(urls); 
+                    
+                    if (progress != null)
+                    {
+                    progress.Report((double) count++ / 10);
+                    }
+            
+                });
+                return total;
+            });
+        }
+
+
+    }
+}
+
+
---
+++ Lectures/2020.01.07-UnitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020._01._07_UnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Assignment7.methods.DownloadTextRepeatedlyAsync(10, new ProgressBar(), "https://facebook.com", "https://facebook.com").Result);
+        }
+    }
+}
